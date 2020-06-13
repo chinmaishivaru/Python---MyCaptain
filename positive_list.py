bc6f413c389b27f51fb5100499c0bc06e0572496
chinmaishivaru <63471578+chinmaishivaru@users.noreply.github.com>
2020-06-13 20:44:54 +0530

    To print all the positive numbers in a list.

---
+++ positive_list.py
@@ -0,0 +1,14 @@
+l1=[12,-7,5,64,-14]
+l2=[12,14,-95,3]
+print("Input list1 = ",l1)
+print("Output: [", end=" ")
+for i in l1:
+    if i>0:
+        print(i, end=" ")
+print("]")
+print("Input list2 = ",l2)
+print("Output: [", end=" ")
+for i in l2:
+    if i>0:
+        print(i, end=" ")
+print("]")
